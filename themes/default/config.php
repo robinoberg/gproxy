da2ebadb1c161a9e52539e87abb61dd08467b595
Dibya Das <dibyadascool@gmail.com>
2017-05-14 00:54:09 +0530

    fix

--- themes/default/config.php
+++ themes/default/config.php
@@ -37,7 +37,9 @@ OUT;
 
 // Proxy description text
 $themeReplace['description'] = <<<OUT
-   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+   <p>Your very own proxy deployed on your Heroku account. </br> <h2><b><font color="red" >Please note that this proxy is not foolproof. Do not use this to do sensitive things where you do not want your identity compromised! </font></b></h2> 
+   	   While this may not work with many sites (like Youtube), it works just fine for many others. This is especially useful for students inside college campus where many sites are blocked by their proxy servers. 
+   </p>
 OUT;
 
 // Ad location above the form on the index page
