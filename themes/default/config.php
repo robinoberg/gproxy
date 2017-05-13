ca3d9f555a803e4630bb4fde3266a5e1cd306630
Dibya Das <dibyadascool@gmail.com>
2017-05-14 01:50:18 +0530

    fix

--- themes/default/config.php
+++ themes/default/config.php
@@ -37,7 +37,9 @@ OUT;
 
 // Proxy description text
 $themeReplace['description'] = <<<OUT
-   <p>Your very own proxy deployed on your Heroku account. </br> <h2><b><font color="red" >Please note that this proxy is not foolproof. Do not use this to do sensitive things where you do not want your identity compromised! </font></b></h2> 
+   <p>Your very own proxy service deployed on your Heroku account. Since this service serves only you, it is not only fast, also reliable.
+   You may have to allow flash in your browser for certain sites (It is disabled by default).
+   </br> <h2><b><font color="red" >Please note that this proxy is not foolproof. Do not use this to do sensitive things where you do not want your identity compromised! </font></b></h2> 
    	   While this may not work with many sites (like Youtube), it works just fine for many others. This is especially useful for students inside college campus where many sites are blocked by their proxy servers. 
    </p>
 OUT;
