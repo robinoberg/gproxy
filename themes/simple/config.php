fd379a684ac0113b465ea8b13903ab045eacf120
robinoberg <46455377+robinoberg@users.noreply.github.com>
2019-02-05 21:10:52 +0000

    Update config.php

--- themes/simple/config.php
+++ themes/simple/config.php
@@ -19,11 +19,11 @@
 ******************************************************************/
 
 // Website name
-$themeReplace['site_name'] = $_SERVER['HTTP_HOST'].' Glype&reg; proxy';
+$themeReplace['site_name'] = $_SERVER['HTTP_HOST'].' proxy';
 
 // Proxy description text
 $themeReplace['description'] = <<<OUT
-   <p>Your very own proxy deployed on your Heroku account. </br> <h2><b><font color="red" >Please note that this proxy is not foolproof. Do not use this to do sensitive things where you do not want your identity compromised! </font></b></h2> 
-   	   While this may not work with many sites (like Youtube), it works just fine for many others. This is especially useful for students inside college campus where many sites are blocked by their proxy servers. 
+   <p>Robin's proxy on Heroku. </br>
+   	   May not work well with sites such as YouTube, but works fine for many others. This is especially useful to access content that are blocked by proxy servers. 
    </p>
 OUT;
